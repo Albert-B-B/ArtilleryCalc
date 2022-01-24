db436be3066a55eac9ba67c5bc9636e22d90ef2f
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2022-01-24 02:56:09 +0100

    Ver 0.1

--- calculator.js
+++ calculator.js
@@ -5,8 +5,22 @@ let artyPos = [0,0];
 let targetPos = [0,0];
 let spotterPos = [0,0];
 
+let windStrength = 0
+let windAngle = 0
+let windBias = [0,0]
+
+let artyDrawFlag = false;
+let targetDrawFlag = false;
+let spotterDrawFlag = false;
+let canvasHeight = 888;
+let canvasWidth  = 1024;
+let regionHeight = 888;
+let regionWidth  = 1024;
+let mapWidthMeter = 1097*2; //1097 comes from https://foxhole.fandom.com/wiki/Community_Guides/Map_Guide.
+let px2mconst = mapWidthMeter/regionWidth;
 let circleSize = 7;
 let moveState = 0;
+
 function preload() {
 
 }
@@ -16,10 +30,9 @@ function changeRegion(){
 }
 
 function setup() {
-  Height = 1050;
-  Width  = 1200;
+
   activeRegionImage = loadImage('https://i.imgur.com/TRl8sds.png')
-  canvas = createCanvas(Height, Width);
+  canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('sketch-holder');
 }
 
@@ -28,55 +41,53 @@ function dis(x1,y1,x2,y2) {
 }
 
 function px2m(numb){
-  return numb*2;
+  return numb*px2mconst;
 }
 
+function m2px(numb) {
+  return numb/px2mconst;
+}
 function getAngle(y,x){
   angle = Math.atan2(y,x)*180/Math.PI
-  console.log(angle)
   if (angle < 0) {
     angle = 360+angle;
   }
+  angle += 90
+  angle = angle % 360
   return angle
 }
-function saveSettings() {
-  showVector = document.getElementById("vectorCheckbox").checked
-  OBList[activePlanet].image =  document.getElementById("planetSkinsSelect").options[document.getElementById("planetSkinsSelect").selectedIndex].value;
-  document.getElementById("activePlanetPic").src = planetSkinsRaw[OBList[activePlanet].image];
-  timeRatio = document.getElementById("timescaleBox").value*60
-  document.getElementById("timescaleSlider").value = document.getElementById("timescaleBox").value
-  CollisionFlag = document.getElementById("colissionCheckbox").checked
-  OBList[activePlanet].radius = document.getElementById("radiusBox").value
-  document.getElementById("radiusSlider").value = document.getElementById("radiusBox").value
-  OBList[activePlanet].mass = document.getElementById("massBox").value*Math.pow(10,document.getElementById("massExponentBox").value)
-  if (speedChangeFlag) {
-    OBList[activePlanet].speedx = convert.disRTG(document.getElementById("speedxBox").value)*Math.pow(10,document.getElementById("speedxExponentBox").value)
-    OBList[activePlanet].speedy = convert.disRTG(document.getElementById("speedyBox").value)*Math.pow(10,document.getElementById("speedyExponentBox").value)
-    speedChangeFlag = false;
-  }
-  trailList[activePlanet].trailLength = document.getElementById("trailLengthBox").value
-  trailList[activePlanet].trailOn = document.getElementById("trailCheckbox").checked
+
+function updateWind(){
+  windStrength = m2px(document.getElementById("windStrength").value)
+  windAngle = document.getElementById("windDirection").value
+  windAngle -= 90
+  windAngle = windAngle % 360
+  windBias[0] = Math.cos(windAngle*Math.PI/180)*windStrength
+  windBias[1] = Math.sin(windAngle*Math.PI/180)*windStrength
+  console.log(windBias)
 }
 
 function draw() {
   clear();
-  image(activeRegionImage,0, 0)
+  image(activeRegionImage,0, 0,regionWidth,regionHeight)
   //Are any of the circles being placed
-  if (moveState == 1) {artyPos = [mouseX,mouseY]}
-  else if (moveState == 2) {targetPos = [mouseX,mouseY]}
-  else if (moveState == 3) {spotterPos = [mouseX,mouseY]}
+  if (moveState == 1) {artyPos = [mouseX,mouseY];artyDrawFlag = true}
+  else if (moveState == 2) {targetPos = [mouseX,mouseY];targetDrawFlag = true}
+  else if (moveState == 3) {spotterPos = [mouseX,mouseY];spotterDrawFlag = true}
 
+  if (artyDrawFlag){
   fill('#2f7a04');
-  circle(artyPos[0], artyPos[1], circleSize);//Artillery
+  circle(artyPos[0], artyPos[1], circleSize);}//Artillery
+  if (targetDrawFlag){
   fill('#ab0011');
-  circle(targetPos[0], targetPos[1], circleSize);//Target
+  circle(targetPos[0], targetPos[1], circleSize);}//Target
+  if (spotterDrawFlag){
   fill('#98a300');
-  circle(spotterPos[0], spotterPos[1], circleSize);//Spotter
-  document.getElementById("distanceP").innerHTML = "Distance: " + str(int(px2m(dis(targetPos[0],targetPos[1],artyPos[0],artyPos[1]))))+" m"
-  document.getElementById("azimuthP").innerHTML = "Distance: " + str(getAngle(targetPos[1]-artyPos[1], targetPos[0]-artyPos[0])) + " deg"
+  circle(spotterPos[0], spotterPos[1], circleSize);}//Spotter
+  document.getElementById("distanceP").innerHTML = "Distance: " + str(int(px2m(dis(targetPos[0]-windBias[0],targetPos[1]-windBias[1],artyPos[0],artyPos[1]))))+" m"
+  document.getElementById("azimuthP").innerHTML = "Azimuth: " + str(Math.round(getAngle(targetPos[1]-windBias[1]-artyPos[1], targetPos[0]-windBias[0]-artyPos[0])*10)/10) + " deg"
 }
 
 function mousePressed(){
-  console.log([mouseX,mouseY])
   moveState = 0;
 }
